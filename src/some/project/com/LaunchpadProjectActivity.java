742c22f2c522864f199af0274888f6078ae82915
Matthew Ito <mito81@gmail.com>
2011-11-17 02:31:09 -0800

    First commit.

---
+++ src/some/project/com/LaunchpadProjectActivity.java
@@ -0,0 +1,13 @@
+package some.project.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LaunchpadProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
