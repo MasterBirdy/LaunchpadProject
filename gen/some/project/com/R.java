061e378731a46e854b468fb1030852444e90e625
Ashwin Amit <ashwinamti@gmail.com>
2011-11-19 18:31:32 -0800

    Fixed package decleration in Event.java

--- gen/some/project/com/R.java
+++ gen/some/project/com/R.java
@@ -46,8 +46,9 @@ public final class R {
     public static final class layout {
         public static final int addevent=0x7f030000;
         public static final int calendar=0x7f030001;
-        public static final int jjjj=0x7f030002;
-        public static final int main=0x7f030003;
+        public static final int fghgd=0x7f030002;
+        public static final int jjjj=0x7f030003;
+        public static final int main=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
--- src/some/project/com/Event.java
+++ src/some/project/com/Event.java
@@ -1,4 +1,3 @@
-package i.like.pizza;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
