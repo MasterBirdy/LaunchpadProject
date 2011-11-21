bfdeac3e1a00c732e850554431fc286f70b916e5
Matthew Ito <mito81@gmail.com>
2011-11-20 18:11:20 -0800

    Added ViewFlipper/alternative layout

--- src/some/project/com/LaunchpadProjectActivity.java
+++ src/some/project/com/LaunchpadProjectActivity.java
@@ -2,6 +2,7 @@ package some.project.com;
 
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.Set;
 
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
@@ -20,102 +21,153 @@ import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 public class LaunchpadProjectActivity extends Activity {
-	
-	private String month;
-	private String day;
-	private String year;
-	private TextView textDate;
-	private TextView textDate1;
+
+	private TextView textDate; // date text
+	private TextView textDate1; // alternative layout date text
 	private Button week1;
-    private Button yearButton;
-    private Button monthButton;
-    private static final int SWIPE_MIN_DISTANCE = 120;
-    private static final int SWIPE_MAX_OFF_PATH = 250;
+	private Button yearButton; 
+	private Button monthButton;
+
+	// stuff for detecting swipes
+	private static final int SWIPE_MIN_DISTANCE = 120;
+	private static final int SWIPE_MAX_OFF_PATH = 250;
 	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 	private GestureDetector gestureDetector;
 	View.OnTouchListener gestureListener;
+
+	// xml animations files for making transitions
 	private Animation slideLeftIn;
 	private Animation slideLeftOut;
 	private Animation slideRightIn;
-    private Animation slideRightOut;
-    private ViewFlipper viewFlipper;
-    private EditText editText1;
-    Calendar c = Calendar.getInstance(); 
-    
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-         monthButton = (Button) findViewById(R.id.button1);
-         yearButton = (Button) findViewById(R.id.buttonYes);
-         textDate = (TextView) findViewById(R.id.date);
-         textDate1 = (TextView) findViewById(R.id.date1);
-         viewFlipper = (ViewFlipper) findViewById(R.id.flipper);
-         monthButton.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US));
-         yearButton.setText(c.get(Calendar.YEAR) + "");
-        textDate.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US) + " " + c.get(Calendar.DATE) + ", " +c.get(Calendar.YEAR));
-        week1 = (Button)findViewById(R.id.button2);
-        editText1 = (EditText) findViewById(R.id.editText1);
-        editText1.setFocusable(false);
-        slideLeftIn = AnimationUtils.loadAnimation(this, R.anim.slide_left_in);
-        slideLeftOut = AnimationUtils.loadAnimation(this, R.anim.slide_left_out);
-        slideRightIn = AnimationUtils.loadAnimation(this, R.anim.slide_right_in);
-        slideRightOut = AnimationUtils.loadAnimation(this, R.anim.slide_right_out);
-        monthButton.setOnClickListener(new View.OnClickListener(){
-        	public void onClick( View v) {
-        		Intent i = new Intent();
-        		i.setClassName("some.project.com",
-        		"some.project.com.AddEvent");
-        		startActivity(i);
-        	}
-        });
-        gestureDetector = new GestureDetector(new MyGestureDetector());
-        gestureListener = new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                if (gestureDetector.onTouchEvent(event)) {
-                    return true;
-                }
-                return false;
-            }
-        };
-    }
-    
-    class MyGestureDetector extends SimpleOnGestureListener {
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            try {
-                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
-                    return false;
-                // right to left swipe
-                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                	viewFlipper.setInAnimation(slideLeftIn);
-                    viewFlipper.setOutAnimation(slideLeftOut);
-                	viewFlipper.showNext();
-                	c.set(5,  c.get(Calendar.DATE)+1);
-                	 textDate.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US) + " " + c.get(Calendar.DATE) + ", " +c.get(Calendar.YEAR));
-                	 textDate1.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US) + " " + c.get(Calendar.DATE) + ", " +c.get(Calendar.YEAR));
-                }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                	viewFlipper.setInAnimation(slideRightIn);
-                    viewFlipper.setOutAnimation(slideRightOut);
-                	viewFlipper.showPrevious();
-                	c.set(5,  c.get(Calendar.DATE)-1);
-                	 textDate.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US) + " " + c.get(Calendar.DATE) + ", " +c.get(Calendar.YEAR));
-                	 textDate1.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US) + " " + c.get(Calendar.DATE) + ", " +c.get(Calendar.YEAR));
-                	}
-            } catch (Exception e) {
-                // nothing
-            }
-            return false;
-        }
-    }
-    
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (gestureDetector.onTouchEvent(event))
-	        return true;
-	    else
-	    	return false;
-    }
+	private Animation slideRightOut;
 
+	//ViewFlipper flips through two layouts in our code. It is how the dates smoothly transition
+	private ViewFlipper viewFlipper;
+
+	// these textfields will hold the information on the events
+	private EditText editText1;
+	private EditText editText2;
+
+	private boolean isAlternativeLayoutOff = true; // checks to see if the 2nd layout in ViewFlipper is being used
+	Calendar c = Calendar.getInstance(); 
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);  //boilerplate code
+		setContentView(R.layout.main);
+
+		monthButton = (Button) findViewById(R.id.button1);         // R.id.findStuff
+		yearButton = (Button) findViewById(R.id.buttonYes);
+		textDate = (TextView) findViewById(R.id.date);
+		textDate1 = (TextView) findViewById(R.id.date1);
+		viewFlipper = (ViewFlipper) findViewById(R.id.flipper);
+		week1 = (Button)findViewById(R.id.button2);
+		editText1 = (EditText) findViewById(R.id.editText1);
+		editText2 = (EditText) findViewById(R.id.editText2);
+
+		monthButton.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US)); // sets the month button to the current month
+		yearButton.setText(c.get(Calendar.YEAR) + ""); // year button same thing
+		textDate.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US) + " " + c.get(Calendar.DATE) + ", " +c.get(Calendar.YEAR));
+
+		editText1.setFocusable(false); // makes it so you can't edit the text fields
+		editText2.setFocusable(false);
+		editText1.setText(returnEvents());
+
+		//loads xml animations
+		slideLeftIn = AnimationUtils.loadAnimation(this, R.anim.slide_left_in);
+		slideLeftOut = AnimationUtils.loadAnimation(this, R.anim.slide_left_out);
+		slideRightIn = AnimationUtils.loadAnimation(this, R.anim.slide_right_in);
+		slideRightOut = AnimationUtils.loadAnimation(this, R.anim.slide_right_out);
+
+		// goes to addEvent activity
+		monthButton.setOnClickListener(new View.OnClickListener(){
+			public void onClick( View v) {
+				Intent i = new Intent();
+				i.setClassName("some.project.com",
+						"some.project.com.AddEvent");
+				startActivity(i);
+			}
+		});
+
+		//detects for swipes
+		gestureDetector = new GestureDetector(new MyGestureDetector());
+		gestureListener = new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (gestureDetector.onTouchEvent(event)) {
+					return true;
+				}
+				return false;
+			}
+		};
+	}
+
+	class MyGestureDetector extends SimpleOnGestureListener {
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			try {
+				if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+					return false;
+
+				// -> swipe
+				if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					viewFlipper.setInAnimation(slideLeftIn);
+					viewFlipper.setOutAnimation(slideLeftOut);
+					viewFlipper.showNext();
+					c.set(5,  c.get(Calendar.DATE)+1); // increase day by one
+
+					if(isAlternativeLayoutOff){ // if 1st layout is being used
+						textDate1.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US) + " " + c.get(Calendar.DATE) + ", " +c.get(Calendar.YEAR));
+						editText2.setText(returnEvents());
+						isAlternativeLayoutOff = false;
+
+					} else { //if 2nd layout is being used
+						textDate.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US) + " " + c.get(Calendar.DATE) + ", " +c.get(Calendar.YEAR));
+						editText1.setText(returnEvents());
+						isAlternativeLayoutOff = true;
+					}
+					//<- swipe
+				}  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					viewFlipper.setInAnimation(slideRightIn);
+					viewFlipper.setOutAnimation(slideRightOut);
+					viewFlipper.showPrevious();
+					c.set(5,  c.get(Calendar.DATE)-1); //decrease day by one
+
+					if(isAlternativeLayoutOff){
+						textDate1.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US) + " " + c.get(Calendar.DATE) + ", " +c.get(Calendar.YEAR));
+						editText2.setText(returnEvents());
+						isAlternativeLayoutOff = false;
+
+					} else {
+						textDate.setText(c.getDisplayName(Calendar.MONTH, Calendar.LONG,  Locale.US) + " " + c.get(Calendar.DATE) + ", " +c.get(Calendar.YEAR));
+						editText1.setText(returnEvents());
+						isAlternativeLayoutOff = true;
+					}
+				}
+			} catch (Exception e) {
+				// nothing
+			}
+			return false;
+		}
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (gestureDetector.onTouchEvent(event))
+			return true;
+		else
+			return false;
+	}
+
+	private String returnDate(int year, int month, int day){ //formats date for sql
+		return year + "-" + month + "-" + day;
+	}
+
+	private String returnEvents(){
+
+		//if (Event.getMatchingEvents("Date", returnDate(c.get(Calendar.YEAR), c.get(Calendar.MONTH)+1, c.get(Calendar.DATE))).get(0).getName() == null)
+		return "No events";
+		//else return Event.getMatchingEvents("Date", returnDate(c.get(Calendar.YEAR), c.get(Calendar.MONTH)+1, c.get(Calendar.DATE))).get(0).getName();
+	}
 }
+
